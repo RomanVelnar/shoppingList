d789e2c2148641ed1eaf3d7cc92d005299e80441
RomanVelnar <roman.velnar@gmail.com>
2022-01-25 16:25:54 +0000

    adding create functions

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 //Selectors
+const shopInput = document.querySelector('.shop-input');
+const shopButton = document.querySelector('.shop-button');
+const shopList = document.querySelector('.shop-list');
 
 //Event Listeners
+shopButton.addEventListener('click', addShop);
 
 //Functions
+function addShop(event){
+    //prevent form from submitting
+    event.preventDefault();
+    //create shop Div
+    const shopDiv = document.createElement("div");
+    shopDiv.classList.add("shop");
+    //create li
+    const newShop = document.createElement('li');
+    newShop.innerText = 'yo';
+    newShop.classList.add('shop-item');
+    shopDiv.appendChild(newShop);
+    //adding completed button
+    const completeButton = document.createElement('button');
+    completeButton.innerHTML = '<i class="fas fa-check"></i>';
+    completeButton.classList.add("complete-btn");
+    shopDiv.appendChild(completeButton);
+    //adding delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add("complete-btn");
+    shopDiv.appendChild(deleteButton);
+    //append to list
+    shopList.appendChild(deleteButton);
+}
